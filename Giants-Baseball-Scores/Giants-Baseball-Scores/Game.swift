40cf98654ef963473b5edb20d20cb4851d33a925
Joe <squirlyproductions@gmail.com>
2020-07-27 13:38:44 -0400

    Master Push

--- Giants-Baseball-Scores/Giants-Baseball-Scores/Game.swift
+++ Giants-Baseball-Scores/Giants-Baseball-Scores/Game.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+
+//MARK:  https://api.sportsdata.io/v3/mlb/scores/json/GamesByDate/{date}
+
+struct Game: Decodable, Encodable {
+    
+    var day: Date
+    var gameID: Int32
+    var season: Int32
+    var homeTeam: String
+    var awayTeam: String
+    var stadiumID: Int32
+    var homeTeamRuns: Int32
+    var awayTeamRuns: Int32
+    
+    
+    enum GameCodingKeys: String, CodingKey {
+        case Day, GameID, Season, HomeTeam, AwayTeam, StadiumID, HomeTeamRuns, AwayTeamRuns
+        
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: GameCodingKeys.self)
+        let day = try container.decode(Date.self, forKey: .Day)
+        let gameID = try container.decode(Int32.self, forKey: .GameID)
+        let season = try container.decode(Int32.self, forKey: .Season)
+        let homeTeam = try container.decode(String.self, forKey: .HomeTeam)
+        let awayTeam = try container.decode(String.self, forKey: .AwayTeam)
+        let stadiumID = try container.decode(Int32.self, forKey: .StadiumID)
+        let homeTeamRuns = try container.decode(Int32.self, forKey: .HomeTeamRuns)
+        let awayTeamRuns = try container.decode(Int32.self, forKey: .AwayTeamRuns)
+        
+        self.day = day
+        self.gameID = gameID
+        self.season = season
+        self.homeTeam = homeTeam
+        self.awayTeam = awayTeam
+        self.stadiumID = stadiumID
+        self.homeTeamRuns = homeTeamRuns
+        self.awayTeamRuns = awayTeamRuns
+        
+    }
+}
