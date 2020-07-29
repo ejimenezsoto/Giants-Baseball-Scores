33a3710f484e88037f73b5b9c1ae9314ab9a1fe2
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-07-29 15:27:55 -0700

    Hide labels for days without Games

--- Giants-Baseball-Scores/Giants-Baseball-Scores/View Controllers/GameViewController.swift
+++ Giants-Baseball-Scores/Giants-Baseball-Scores/View Controllers/GameViewController.swift
@@ -30,7 +30,11 @@ class GameViewController: UIViewController {
             }
         }
     }
+    @IBOutlet weak var homeLabel: UILabel!
+    @IBOutlet weak var awayLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
     
+    @IBOutlet weak var noGamesLabel: UILabel!
     // Home Team Labels
     @IBOutlet weak var homeImageView: UIImageView!
     @IBOutlet weak var homeStandingsLabel: UILabel!
@@ -77,102 +81,129 @@ class GameViewController: UIViewController {
     }
     var filteredGames = [Game]()
     
+    var noGamesOnDayArray = ["2020-07-22T00:00:00", "2020-07-27T00:00:00", "2020-08-13T00:00:00","2020-08-24T00:00:00","2020-08-31T00:00:00", "2020-09-03T00:00:00", "2020-09-14T00:00:00","2020-09-17T00:00:00","2020-09-28T00:00:00", "2020-09-29T00:00:00", "2020-09-30T00:00:00"]
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        gameController.fetchGiantsSchedule { (result) in
-            
-            self.giantsGames = self.gameController.gotDamnGiantsGames
-            let giantsAwayGames = self.giantsGames.filter { $0.awayTeamName == "SF"}
-            let giantsHomeGames = self.giantsGames.filter { $0.homeTeamName == "SF"}
-            
-            let allGiantsGames = giantsAwayGames + giantsHomeGames
-            
-            let filterDateGiantsGames = allGiantsGames.filter { $0.day == self.searchDateString }
-            //print(filterDateGiantsGames)
-            
-            
-            self.filteredGames = filterDateGiantsGames
-            
-            print(self.filteredGames)
-            
-            DispatchQueue.main.async {
+        
+        if noGamesOnDayArray.contains(searchDateString) {
+            self.homeImageView.isHidden = true
+            self.homeTeamNameLabel.isHidden = true
+            self.gameScoreLabel.isHidden = true
+            self.homeLabel.isHidden = true
+            self.scoreLabel.isHidden = true
+            self.awayLabel.isHidden = true
+            self.awayImageView.isHidden = true
+            self.awayTeamNameLabel.isHidden = true
+            self.stadiumButton.isHidden = true
+            self.noGamesLabel.isHidden = false
+        } else {
+            gameController.fetchGiantsSchedule { (result) in
                 
-                self.homeTeamNameLabel.text = self.filteredGames.first?.homeTeamName
-                self.awayTeamNameLabel.text = self.filteredGames.first?.awayTeamName
+                self.giantsGames = self.gameController.gotDamnGiantsGames
+                let giantsAwayGames = self.giantsGames.filter { $0.awayTeamName == "SF"}
+                let giantsHomeGames = self.giantsGames.filter { $0.homeTeamName == "SF"}
                 
-                if self.filteredGames.first?.homeTeamName == "SF" {
-                    self.homeImageView.image = UIImage(named: "San Francisco Giants")
-                }
-                if self.filteredGames.first?.homeTeamName == "LAD" {
-                    self.homeImageView.image = UIImage(named: "Los Angeles Dodgers")
-                }
-                if self.filteredGames.first?.homeTeamName == "SD" {
-                    self.homeImageView.image = UIImage(named: "San Diego Padres")
-                }
-                if self.filteredGames.first?.homeTeamName == "TEX" {
-                    self.homeImageView.image = UIImage(named: "Texas Rangers")
-                }
-                if self.filteredGames.first?.homeTeamName == "COL" {
-                    self.homeImageView.image = UIImage(named: "Colorado Rockies")
-                }
-                if self.filteredGames.first?.homeTeamName == "HOU" {
-                    self.homeImageView.image = UIImage(named: "Houston Astros")
-                }
-                if self.filteredGames.first?.homeTeamName == "OAK" {
-                    self.homeImageView.image = UIImage(named: "Oakland Athletic")
-                }
-                if self.filteredGames.first?.homeTeamName == "LAA" {
-                    self.homeImageView.image = UIImage(named: "Los Angeles Angels")
-                }
-                if self.filteredGames.first?.homeTeamName == "ARI" {
-                    self.homeImageView.image = UIImage(named: "Arizona Diamondbacks")
-                }
-                if self.filteredGames.first?.homeTeamName == "SEA" {
-                    self.homeImageView.image = UIImage(named: "Seattle Mariners")
-                }
-                if self.filteredGames.first?.awayTeamName == "SF" {
-                    self.awayImageView.image = UIImage(named: "San Francisco Giants")
-                }
-                if self.filteredGames.first?.awayTeamName == "LAD" {
-                    self.awayImageView.image = UIImage(named: "Los Angeles Dodgers")
-                }
-                if self.filteredGames.first?.awayTeamName == "SD" {
-                    self.awayImageView.image = UIImage(named: "San Diego Padres")
-                }
-                if self.filteredGames.first?.awayTeamName == "TEX" {
-                    self.awayImageView.image = UIImage(named: "Texas Rangers")
-                }
-                if self.filteredGames.first?.awayTeamName == "COL" {
-                    self.awayImageView.image = UIImage(named: "Colorado Rockies")
-                }
-                if self.filteredGames.first?.awayTeamName == "HOU" {
-                    self.awayImageView.image = UIImage(named: "Houston Astros")
-                }
-                if self.filteredGames.first?.awayTeamName == "OAK" {
-                    self.awayImageView.image = UIImage(named: "Oakland Athletic")
-                }
-                if self.filteredGames.first?.awayTeamName == "LAA" {
-                    self.awayImageView.image = UIImage(named: "Los Angeles Angels")
-                }
-                if self.filteredGames.first?.awayTeamName == "ARI" {
-                    self.awayImageView.image = UIImage(named: "Arizona Diamondbacks")
-                }
-                if self.filteredGames.first?.awayTeamName == "SEA" {
-                    self.awayImageView.image = UIImage(named: "Seattle Mariners")
-                }
+                let allGiantsGames = giantsAwayGames + giantsHomeGames
                 
-              let homeTeamRuns = self.filteredGames.first?.homeTeamRuns
-                let awayTeamRuns = self.filteredGames.first?.awayTeamRuns
-               
-                let gameRuns = "\(homeTeamRuns ?? 0) - \(awayTeamRuns ?? 0)"
+                let filterDateGiantsGames = allGiantsGames.filter { $0.day == self.searchDateString }
+                //print(filterDateGiantsGames)
                 
-                self.gameScoreLabel.text = gameRuns
                 
-                print(gameRuns)
+                self.filteredGames = filterDateGiantsGames
+                
+                print(self.filteredGames)
+                
+                DispatchQueue.main.async {
+                    
+                    self.homeTeamNameLabel.text = self.filteredGames.first?.homeTeamName
+                    self.awayTeamNameLabel.text = self.filteredGames.first?.awayTeamName
+                    
+                    if self.filteredGames.first?.homeTeamName == "SF" {
+                        self.homeImageView.image = UIImage(named: "San Francisco Giants")
+                    }
+                    if self.filteredGames.first?.homeTeamName == "LAD" {
+                        self.homeImageView.image = UIImage(named: "Los Angeles Dodgers")
+                    }
+                    if self.filteredGames.first?.homeTeamName == "SD" {
+                        self.homeImageView.image = UIImage(named: "San Diego Padres")
+                    }
+                    if self.filteredGames.first?.homeTeamName == "TEX" {
+                        self.homeImageView.image = UIImage(named: "Texas Rangers")
+                    }
+                    if self.filteredGames.first?.homeTeamName == "COL" {
+                        self.homeImageView.image = UIImage(named: "Colorado Rockies")
+                    }
+                    if self.filteredGames.first?.homeTeamName == "HOU" {
+                        self.homeImageView.image = UIImage(named: "Houston Astros")
+                    }
+                    if self.filteredGames.first?.homeTeamName == "OAK" {
+                        self.homeImageView.image = UIImage(named: "Oakland Athletic")
+                    }
+                    if self.filteredGames.first?.homeTeamName == "LAA" {
+                        self.homeImageView.image = UIImage(named: "Los Angeles Angels")
+                    }
+                    if self.filteredGames.first?.homeTeamName == "ARI" {
+                        self.homeImageView.image = UIImage(named: "Arizona Diamondbacks")
+                    }
+                    if self.filteredGames.first?.homeTeamName == "SEA" {
+                        self.homeImageView.image = UIImage(named: "Seattle Mariners")
+                    }
+                    if self.filteredGames.first?.awayTeamName == "SF" {
+                        self.awayImageView.image = UIImage(named: "San Francisco Giants")
+                    }
+                    if self.filteredGames.first?.awayTeamName == "LAD" {
+                        self.awayImageView.image = UIImage(named: "Los Angeles Dodgers")
+                    }
+                    if self.filteredGames.first?.awayTeamName == "SD" {
+                        self.awayImageView.image = UIImage(named: "San Diego Padres")
+                    }
+                    if self.filteredGames.first?.awayTeamName == "TEX" {
+                        self.awayImageView.image = UIImage(named: "Texas Rangers")
+                    }
+                    if self.filteredGames.first?.awayTeamName == "COL" {
+                        self.awayImageView.image = UIImage(named: "Colorado Rockies")
+                    }
+                    if self.filteredGames.first?.awayTeamName == "HOU" {
+                        self.awayImageView.image = UIImage(named: "Houston Astros")
+                    }
+                    if self.filteredGames.first?.awayTeamName == "OAK" {
+                        self.awayImageView.image = UIImage(named: "Oakland Athletic")
+                    }
+                    if self.filteredGames.first?.awayTeamName == "LAA" {
+                        self.awayImageView.image = UIImage(named: "Los Angeles Angels")
+                    }
+                    if self.filteredGames.first?.awayTeamName == "ARI" {
+                        self.awayImageView.image = UIImage(named: "Arizona Diamondbacks")
+                    }
+                    if self.filteredGames.first?.awayTeamName == "SEA" {
+                        self.awayImageView.image = UIImage(named: "Seattle Mariners")
+                    }
+                    
+                    let homeTeamRuns = self.filteredGames.first?.homeTeamRuns
+                    let awayTeamRuns = self.filteredGames.first?.awayTeamRuns
+                    
+                    let gameRuns = "\(homeTeamRuns ?? 0) - \(awayTeamRuns ?? 0)"
+                    
+                    self.gameScoreLabel.text = gameRuns
+                    self.homeImageView.isHidden = false
+                               self.homeTeamNameLabel.isHidden = false
+                               self.gameScoreLabel.isHidden = false
+                               self.homeLabel.isHidden = false
+                               self.scoreLabel.isHidden = false
+                               self.awayLabel.isHidden = false
+                               self.awayImageView.isHidden = false
+                               self.awayTeamNameLabel.isHidden = false
+                               self.stadiumButton.isHidden = false
+                    
+                    self.noGamesLabel.isHidden = true
+                    
+                    print(gameRuns)
+                }
+            }
         }
-    }
     }
     private func formatDate(date: Date) -> String {
         let dateFormatter = DateFormatter()
