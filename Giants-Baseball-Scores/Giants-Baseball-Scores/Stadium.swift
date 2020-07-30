ebf67de028a6658a50affc314cc1c0e5488ab660
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-07-29 19:15:13 -0700

    Created Stadium struct with all info

---
+++ Giants-Baseball-Scores/Giants-Baseball-Scores/Stadium.swift
@@ -0,0 +1,9 @@
+//
+//  Stadium.swift
+//  Giants-Baseball-Scores
+//
+//  Created by Enzo Jimenez-Soto on 7/29/20.
+//  Copyright © 2020 Enzo Jimenez-Soto. All rights reserved.
+//
+
+import Foundation
